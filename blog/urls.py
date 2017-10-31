e031bff54e047ee41b283082cd85af59e1c79359
sjw0484 <sjw0484@naver.com>
2017-10-31 19:20:19 +0900

    greeting CBV 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-
+	url(r'^$', views.greeting),
+	url(r'^morning/$', views.morning_greeting),
+	url(r'^evening/$', views.evening_greeting),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+class GreetingView(View):
+	message = "good day"
+	def get(self, *args, **kwargs):
+		return HttpResponse(self.message)
+
+greeting = GreetingView.as_view()
+
+class MorningGreetingView(GreetingView):
+	message = 'morning to ya'
+
+morning_greeting = MorningGreetingView.as_view()
+
+evening_greeting = GreetingView.as_view(message='eveing to ya')
